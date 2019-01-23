12ddb40303e4bbe526539ef519a9c275c1fb8ea4
David Cote <davesdere@users.noreply.github.com>
2019-01-22 21:58:33 -0500

    update cheatsheet.sh

--- cheatsheet.sh
+++ cheatsheet.sh
@@ -8,16 +8,16 @@ echo "INFO  : $progName $ver  written by David Cote | MIT 2018"
 username=$1
 groupname=$2
 if $1="":
-  echo "You need 2 arguments username and groupname"
+  echo "You need 2 arguments username and groupname to set them " & echo "now it's just a log monster"
 fi
 # Accesses
 # https://help.ubuntu.com/community/Sudoers # Good ressource
 # Add a user and set their password
-sudo adduser $username
+#sudo adduser $username
 # What groups are there?
 groups
 # To add to the groups, First let's see which one 'pi' has
-sudo adduser $username $groupName
+#sudo adduser $username $groupName
 
 #sudo dpkg-reconfigure tzdata # Setting timezone data
 
@@ -60,10 +60,28 @@ hostnamectl | grep "Operating System" # To get the code name
 
 # Magic
 ## Ziping files
-find . -name "*.[jpg]" -print | zip source -@ # Will put all the JPG of the pi in a zip
+#find . -name "*.[jpg]" -print | zip source -@ # Will put all the JPG of the pi in a zip
+
+touch file.csv
+list_of_names='Lisa Homer Bart Stan Kyle Cartman'
+append_a_list () {
+  for name in $names
+  do
+    echo $name, >> file.csv
+  done
+}
+append_a_list
+# 3 times in a file.csv
+for i in {1..5}
+do
+  append_a_list
+done
+cat file.csv
 ## Remove lines from files
 # This removes lines from a file where a value between , is less than 12 characters
 awk -F "," 'length($2)>12' file.csv
+rm file.csv | sudo !!
+
 # Lazy ListFiles
 alias l="ls -a"
 echo "alias la='ls -A'" > ~/.bashrc
@@ -71,5 +89,41 @@ echo "alias la='ls -A'" > ~/.bashrc
 source ~/..bashrc
 
 # To check the connection from ssh
-# Can be used to trigger backup or restore point
+# Can be used to trigger backup or restore point with a listener
 #sudo cat /var/log/auth.log
+
+# Processes
+ps aux | grep nginx
+ps aux | grep docker
+ls /mnt
+
+# Blocks
+lsblk
+
+# Processing power
+lscpu
+
+# Mount info
+#man mount
+mount | column -t
+
+# Mount sda3
+#mount /dev/sda3 /mnt
+
+# Re-do last command in sudo
+sudo !!
+
+# Logs
+find /var/log
+
+# Traceroute
+#mtr --curses 192.168.1.1
+
+# Disk usage
+df -H
+
+#Partition info
+fdisk -l
+
+# reset
+reset
