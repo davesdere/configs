ddf7c79f646c9894a8b51dd4df4897af4c87abf5
David Cote <davesdere@users.noreply.github.com>
2020-12-15 12:02:13 -0500

    Update set_me_up.sh

--- set_me_up.sh
+++ set_me_up.sh
@@ -1,6 +1,11 @@
 alias lg="git log --all --decorate --oneline --graph"
-parse_git_branch() {
-     git branch >2 /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (/1)/'
+git_branch() {
+    git branch 2>/dev/null | grep '^*' | colrm 1 2
 }
 
-PS1='\D{%T} $(parse_git_branch)\n${PS1}'
+
+
+# Two lines ps1
+PS1='\D{%T} ${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]\[\033[01;34m\]\n\[\033[00m\]└── \[\033[01;34m\]\w\[\033[00m\]\$ '
+# Add git to ps1
+PS1='\D{%T} ${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]\[\033[01;34m\] ( $(git_branch) )\n\[\033[00m\]└── \[\033[01;34m\]\w\[\033[00m\]\$ '
