91393cf57bd343059597b7fe05d770614dffc477
David Cote <davesdere@users.noreply.github.com>
2020-12-17 15:16:12 -0500

    Update bashrc.sh

--- bashrc.sh
+++ bashrc.sh
@@ -1,11 +1,12 @@
 parse_git_branch() {
-    f='└──'
+    f='└── '
     g=$(git branch 2>/dev/null | grep '^*' | colrm 1 2)
-    if [ -z "$var" ]
+    if [ -z "$g" ]
     then
       echo
     else
-      echo '\n$f[$g]'
+      echo 
+      echo $f$g
     fi
 }
 PS1='\D{%T} ${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]\[\033[01;34m\] $(parse_git_branch)\n\[\033[00m\]└── \[\033[01;34m\]\w\[\033[00m\]\$ '
